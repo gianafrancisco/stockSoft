619849ed4db26951026e0aa08716ca25659ac4a8
francisco <gianafrancisco@gmail.com>
2016-06-21 22:01:28 -0300

    removed old javascript code.

--- src/main/resources/static/js/articuloApp.js
+++ src/main/resources/static/js/articuloApp.js
@@ -21,37 +21,15 @@ function articuloController($scope,$http,$window,$location, Restangular) {
         if($scope.search != "" && $scope.search != undefined){
             params.search = $scope.search;
         }
-     /*
-         var url = "/articulos?page="+($scope.pageNumber-1);
-         if($scope.search != "" && $scope.search != undefined){
-             var search = "&search="+$scope.search;
-             url = "/articulo/search?page="+($scope.pageNumber-1)+search;
-         }
-         $http.get(url)
-         .success(function(data, status, headers, config) {
-             $scope.listado=data;
-             $scope.pageNumber = $scope.listado.number+1;
-         });
-         */
-
-         Articulo.getList(params).then(function(a){
+        Articulo.getList(params).then(function(a){
              $scope.listado = a;
              $scope.pageNumber = $scope.listado.number+1;
-         });
+        });
 
      };
 
      $scope.buscarArticulo = function(){
          $scope.obtenerListaArticulo();
-     };
-
-     $scope.obtenerListaCodigo = function(articulo){
-         if(articulo.articuloId != undefined){
-             $http.get("/articulo/"+articulo.articuloId+"/items")
-             .success(function(data, status, headers, config) {
-                 $scope.listadoCodigo=data;
-             });
-          }
      };
 
      $scope.agregarArticulo = function(){
@@ -66,21 +44,9 @@ function articuloController($scope,$http,$window,$location, Restangular) {
              $scope.articulo = {};
          });
      };
-
-     $scope.agregarCodigo = function(){
-         $scope.codigo.itemId = null;
-         $scope.saveItem();
-     };
-
-     $scope.modificarCodigo = function(){
-         $scope.saveItem();
-     };
-
      $scope.modificar = function(articulo){
          $scope.articulo = articulo;
-         //$scope.obtenerListaCodigo($scope.articulo);
      };
-
      $scope.copiar = function(articulo){
          $scope.modificar(articulo);
          $scope.articulo.articuloId = undefined;
@@ -92,8 +58,11 @@ function articuloController($scope,$http,$window,$location, Restangular) {
              element.focus();
          }
      };
-
      $scope.save = function(callback){
+
+        //$scope.articulo.put();
+
+     /*
          $http.put("/articulo/agregar",$scope.articulo)
          .success(function(data, status, headers, config) {
              $scope.articulo=data;
@@ -102,47 +71,16 @@ function articuloController($scope,$http,$window,$location, Restangular) {
                  callback();
              }
          });
+     */
      };
 
      $scope.eliminar = function(articulo){
-
          if(confirm("Esta seguro que quiere elimiar el articulo?")){
-                  //Articulo.remove(articulo.articuloId).then(function(a){
-                    //a.remove();
-                    //$scope.obtenerListaArticulo();
-                  //});
                   Restangular.one("articulos", articulo.articuloId).remove();
-/*
-             $http.get("/articulo/delete/"+articulo.articuloId)
-             .success(function(data, status, headers, config) {
-                 $scope.obtenerListaArticulo();
-                 //$scope.obtenerListaCodigo(articulo);
-             });*/
          }
      };
-
-     $scope.eliminarCodigo = function(articulo,item){
-
-         var a = Articulo.get(item.itemId);
-         a.remove();
-
-         /*$http.get("/articulo/"+articulo.articuloId+"/item/delete/"+item.itemId)
-         .success(function(data, status, headers, config) {
-             //$scope.obtenerListaCodigo(articulo);
-         });*/
-     };
-
-     $scope.saveItem = function(){
-         $http.put("/articulo/"+$scope.articulo.articuloId+"/agregar",$scope.codigo)
-         .success(function(data, status, headers, config) {
-             $scope.codigo=data;
-             //$scope.obtenerListaCodigo($scope.articulo);
-         });
-     };
-
      $scope.init = function(){
          $scope.obtenerListaArticulo();
-
      };
 
      $scope.isModificable = function(){
@@ -156,5 +94,4 @@ function articuloController($scope,$http,$window,$location, Restangular) {
          });
      };
      $scope.init();
-
  };
