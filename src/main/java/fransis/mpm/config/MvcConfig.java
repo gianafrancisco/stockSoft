e1e501948d17f4bdafef824c0b5e36adca32ae35
francisco <gianafrancisco@gmail.com>
2016-07-02 13:40:22 -0300

    GUI development workaround in order to use java as web server.

---
+++ src/main/java/fransis/mpm/config/MvcConfig.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016-2016 Francisco Giana <gianafrancisco@gmail.com>
+ *
+ */
+
+package fransis.mpm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by francisco on 6/27/16.
+ */
+@Configuration
+@Component
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/dev/bower_components/**")
+                .addResourceLocations("file:/home/francisco/git/stockSoft/src/main/gui/bower_components/")
+                //.addResourceLocations("classpath:../../src/main/gui/bower_components/")
+                .setCachePeriod(0);
+        registry.addResourceHandler("/dev/**")
+                .addResourceLocations("file:/home/francisco/git/stockSoft/src/main/gui/app/")
+                //.addResourceLocations("classpath:../../src/main/gui/app/")
+                .setCachePeriod(0);
+    }
+}
