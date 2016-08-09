30f9f4b4fe243d2a59eb9565505177127d73c6fb
francisco <gianafrancisco@gmail.com>
2016-08-08 22:09:45 -0300

    Solved filter by username on ReservaRepository.

--- src/main/java/fransis/mpm/repository/ReservaRepository.java
+++ src/main/java/fransis/mpm/repository/ReservaRepository.java
@@ -10,6 +10,7 @@ import fransis.mpm.model.Reserva;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -17,7 +18,10 @@ import java.util.List;
  * Created by francisco on 7/2/16.
  */
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-    //TODO: Create test in order to test if the vendedor filter is working.
+
+    @Query(value = "SELECT r FROM Reserva r WHERE r.vendedor = ?1 AND (r.descripcion LIKE %?2% OR r.email LIKE %?3%) AND estado IN ?4",
+            countQuery = "SELECT count(r) FROM Reserva r WHERE r.vendedor = ?1 AND (r.descripcion LIKE %?2% OR r.email LIKE %?3%) AND estado IN ?4",
+            nativeQuery = false)
     Page<Reserva> findByVendedorAndDescripcionContainingIgnoreCaseOrEmailContainingIgnoreCaseAndEstadoIn(String vendedor, String descripcion, String email, List<EstadoReserva> estado, Pageable pageable);
     Page<Reserva> findByVendedorAndEstadoIn(String vendedor, List<EstadoReserva> estado, Pageable pageable);
 }
