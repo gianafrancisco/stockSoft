88012a3ca412e27f62929244929b020cb72a0baf
Francisco Giana <gianafrancisco@gmail.com>
2016-09-19 10:20:46 -0300

    rc0.1.2

--- installer/run.bat
+++ installer/run.bat
@@ -1,3 +1,3 @@
 @echo off
-REM start "GGSTOCK" .\jre\bin\javaw.exe -jar mpmStock-0.1.1.jar
-.\jre\bin\java.exe -jar mpmStock-0.1.1.jar
+REM start "GGSTOCK" .\jre\bin\javaw.exe -jar mpmStock-0.1.2.jar
+.\jre\bin\java.exe -jar mpmStock-0.1.2.jar
